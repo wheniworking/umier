4f0f10cb03ed3b676775ba53227811b7fb52472d
zdw <zhoudongwei@cvte.com>
2018-07-31 17:31:43 +0800

    init project

---
+++ src/main/java/com/longwei/umier/UmierApplication.java
@@ -0,0 +1,12 @@
+package com.longwei.umier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmierApplication.class, args);
+	}
+}
