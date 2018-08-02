8d51253c27bc75934c04edd99eb7f452a271d9d0
zdw <zhoudongwei@cvte.com>
2018-08-02 15:54:34 +0800

    添加微信事件处理的基础类

---
+++ src/main/java/com/longwei/umier/controller/wx/WXMsgEventController.java
@@ -0,0 +1,28 @@
+package com.longwei.umier.controller.wx;
+
+import com.longwei.umier.service.WXMsgEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api/v1/wx")
+public class WXMsgEventController {
+
+    @Autowired
+    private WXMsgEventService wxMsgEventService;
+
+    @GetMapping(value = "/msg")
+    public String connectWechat(@RequestParam("echostr") String echoStr) {
+        return echoStr;
+    }
+
+    @PostMapping(value = "/msg")
+    public Object receiveEvent(HttpServletRequest request, HttpServletResponse response ) {
+        wxMsgEventService.handleEvent();
+        return null;
+    }
+
+}
---
+++ src/main/java/com/longwei/umier/service/WXMsgEventService.java
@@ -0,0 +1,14 @@
+package com.longwei.umier.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WXMsgEventService {
+
+
+    public Object handleEvent() {
+        //处理微信的事件
+        //完成相关数据的保存即可
+        return null;
+    }
+}
